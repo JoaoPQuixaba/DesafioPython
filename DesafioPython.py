6235b6f7a67ca72280506b64a59266e3b68725e5
JoaoPQuixaba <joaopedrodonascimentonew@gmail.com>
2024-05-27 18:13:46 -0300

    Corrigido o BUG da Função de Verificação do CPF - V5!

--- DesafioPython.py
+++ DesafioPython.py
@@ -2,32 +2,34 @@ import tkinter
 from tkinter import messagebox as mb
 from tkinter import ttk
 import sqlite3
+import re
 
 connection = sqlite3.connect("teste.db")
 
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS Tabela1 (nome TEXT, cpf TEXT, estado TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS Tabela1 (nome TEXT, cpf TEXT, estado TEXT, tipo TEXT)")
 
 def VerificarCPF(CPF):
-    for trecho in CPF.split("."):
-        if len(trecho) != 3:
-            return False
-    return True
+    pattern = re.compile(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$')
+    if pattern.match(CPF):
+        return True
+    else:
+        return False
 
-def inserevalores(nome, cpf, estado):
-    cursor.execute("INSERT INTO Tabela1 (nome, cpf, estado) VALUES (?, ?, ?)", (nome, cpf, estado))
+def inserevalores(nome, cpf, estado, tipo):
+    cursor.execute("INSERT INTO Tabela1 (nome, cpf, estado, tipo) VALUES (?, ?, ?, ?)", (nome, cpf, estado, tipo))
     connection.commit()
 
 def pegavalores():
     rows = cursor.execute("SELECT * FROM Tabela1").fetchall()
     print(rows)
 
-def funcSalvar(nome, cpf, estado):
+def funcSalvar(nome, cpf, estado, tipo):
     if VerificarCPF(cpf):
-        inserevalores(nome, cpf, estado)
+        inserevalores(nome, cpf, estado, tipo)
         mb.showinfo("Informação", "Dados salvos com sucesso!")
     else:
-        mb.showerror("Erro", "CPF inválido!")
+        mb.showerror("Erro", "CPF inválido! Formato correto: 111.111.111-11")
 
 def Main():
     root = tkinter.Tk()
@@ -52,9 +54,16 @@ def Main():
     textoEntradaEstado = tkinter.StringVar()
     e_estado = tkinter.Entry(root, textvariable=textoEntradaEstado)
     e_estado.pack()
+
+    label_tipo = tkinter.Label(root, text="Tipo")
+    label_tipo.pack()
+    tipo_var = tkinter.StringVar()
+    combobox_tipo = ttk.Combobox(root, textvariable=tipo_var)
+    combobox_tipo['values'] = ("CLR", "MEI", "SÓCiO")
+    combobox_tipo.pack()
     
     test2 = tkinter.Button(root, text="Salvar")
-    test2['command'] = lambda: funcSalvar(textoEntradaNome.get(), textoEntradaCPF.get(), textoEntradaEstado.get())
+    test2['command'] = lambda: funcSalvar(textoEntradaNome.get(), textoEntradaCPF.get(), textoEntradaEstado.get(), tipo_var.get())
     test2.pack()
 
     root.iconify()
